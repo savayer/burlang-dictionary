bbaef9a6792f62db9111076a68beeb000fa9f879
savayer <sava0094@gmail.com>
2023-02-16 22:28:20 +0800

    fixed status bar

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './components/Navigation';
 import { StatusBar } from 'expo-status-bar';
 import colors from './constants/colors';
+import { Platform } from 'react-native';
 
 export default function App() {
   return (
     <>
-      <StatusBar backgroundColor={colors.blue} />
+      <StatusBar
+        style={Platform.OS === 'ios' ? 'auto' : 'light'}
+        backgroundColor={colors.blue}
+      />
 
       <NavigationContainer>
         <Navigation />
